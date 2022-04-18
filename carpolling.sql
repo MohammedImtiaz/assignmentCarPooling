d80f162669d0eb8a09fd94071d6edc0366fc9f66
Mohammed Imtiaz <61940000+MohammedImtiaz@users.noreply.github.com>
2022-04-18 15:13:02 +0600

    Update carpolling.sql

--- carpolling.sql
+++ carpolling.sql
@@ -1,21 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.0.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Apr 18, 2022 at 11:07 AM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.0
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `carpolling`
@@ -41,7 +23,3 @@ CREATE TABLE `carpolling` (
 INSERT INTO `carpolling` (`employee_id`, `destination`, `email`, `price`) VALUES
 (101, '32 block', '101@gmail.com', '1000');
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
