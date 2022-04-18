62760a4495f3b2bc417bd73d1b6a9eef5a71c66e
Mohammed Imtiaz <61940000+MohammedImtiaz@users.noreply.github.com>
2022-04-18 18:58:30 +0600

    Add files via upload

--- car.sql
+++ car.sql
@@ -1,6 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 18, 2022 at 02:56 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
 -- Database: `car`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `carpolling`
+--
+
+CREATE TABLE `carpolling` (
+  `employee_id` int(10) NOT NULL,
+  `destination` varchar(20) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `price` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `carpolling`
+--
+
+INSERT INTO `carpolling` (`employee_id`, `destination`, `email`, `price`) VALUES
+(101, '32 block', '101@gmail.com', '1000'),
+(102, '30 block', '102@gmail.com', '2000'),
+(103, '34 block', '103@gmail.com', '5000');
 
 -- --------------------------------------------------------
 
@@ -51,3 +92,6 @@ ALTER TABLE `product`
   MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
