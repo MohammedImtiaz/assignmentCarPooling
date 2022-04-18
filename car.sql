3ed301685a26aa178c9f90d6e23c21650d7f872f
Mohammed Imtiaz <61940000+MohammedImtiaz@users.noreply.github.com>
2022-04-18 15:04:36 +0600

    Add files via upload

---
+++ car.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 18, 2022 at 11:02 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `car`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `pid` int(11) NOT NULL,
+  `name` varchar(70) DEFAULT NULL,
+  `image` varchar(255) NOT NULL,
+  `category` varchar(70) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`pid`, `name`, `image`, `category`, `price`) VALUES
+(1, 'Car1', 'car.jpg', 'Car', 1000),
+(2, 'Car2', 'car.jpg', 'Car', 2000),
+(3, 'Car3', 'car.jpg', 'Car', 3000),
+(4, 'Car4', 'car.jpg', 'Car', 4000),
+(5, 'Car5', 'car.jpg', 'Car', 5000),
+(6, 'Car6', 'car.jpg', 'Car', 5500),
+(7, 'Car7', 'car.jpg', 'Car', 8000),
+(8, 'Car8', 'car.jpg', 'Car', 10000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
